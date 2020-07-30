3edf06a71205fbd148d73eba067d0636919cd284
Alexei <alexeiatyahoodotcom@gmail.com>
2020-07-30 16:40:21 -0400

    Add fixture for test_cookie_tracker_detection

---
+++ html/cookie_frame.js
@@ -0,0 +1,25 @@
+function setExpire() {
+	var now = new Date();
+	var time = now.getTime();
+	var expireTime = time + 864000;
+	now.setTime(expireTime);
+	return ";expires=" + now.toGMTString();
+}
+
+function setPath() {
+	return ";path=/";
+}
+
+function setSameSite() {
+	return ";SameSite=None;Secure";
+}
+
+function updateCookie() {
+	var oldcookie = document.cookie;
+	var val = "1234567890";
+	console.log("read cookie:" + oldcookie);
+	document.cookie = "thirdpartytest=" + encodeURIComponent(val) + setExpire() + setPath() + setSameSite();
+	console.log("updated cookie to:" + document.cookie);
+}
+
+updateCookie();
